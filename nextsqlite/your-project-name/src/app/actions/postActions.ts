c5d6137d869eee2157a88ae1f60193d762ac8fb5
bkhebert <bondman2004tippy@gmail.com>
2025-06-02 08:19:11 -0500

    ADD server action that posts form data to form table

--- nextsqlite/your-project-name/src/app/actions/postActions.ts
+++ nextsqlite/your-project-name/src/app/actions/postActions.ts
@@ -1,3 +1,16 @@
 'use server'; // For server actions specifically, `use server` is required
 
 import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function createPost(formData: FormData) {
+  const title = formData.get('title') as string;
+  const content = formData.get('content') as string;
+
+  await prisma.post.create({
+    data: {
+      title,
+      content,
+    },
+  })
+}
